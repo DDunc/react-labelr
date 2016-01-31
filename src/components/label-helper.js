1113e3082d9eb6efa708795e0756f07227ffe9dc
Duncan <Duncan.David@gmail.com>
2016-01-30 23:08:05 -0800

    Sat Jan 30 23:08:05 PST 2016

--- src/components/label-helper.js
+++ src/components/label-helper.js
@@ -1,12 +1,26 @@
 import React from 'react'
+import ampersandMixin from 'ampersand-react-mixin'
 
 //defining label const
 export default React.createClass({
     displayName: "labelHelper",
 
+    mixins: [ampersandMixin],
+
+
+    //props is what is passed to it and appended
+    onEditClick () {
+        this.props.label.editing = true;
+    },
+    onCancelClick () {
+        this.props.label.editing = false;
+    },
+
     render(){
         const {label} = this.props;
+        const cssColor = "#" + label.color;
         let content;
+
         console.log(label.editing);
         //editing
         if(label.editing) {
@@ -15,13 +29,13 @@ export default React.createClass({
                 <input name='name'/>
                 <input name='color'/>
                 <button type='submit' className='button button-small'>Save</button>
-                <button type='button' className='button button-small button-unstyled'>cancel</button>
+                <button type='button' className='button button-small button-unstyled'  onClick={this.onCancelClick}>cancel</button>
             </form>)
         } else {
           content = (  <div className='label'>
-                <span className='label-color'>&nbsp;</span>
-                <span> </span>
-                <span className='octicon octicon-pencil'> </span>
+                <span className='label-color' style={{backgroundColor: cssColor}}>&nbsp;</span>
+                <span> {label.name}</span>
+                <span className='octicon octicon-pencil' onClick={this.onEditClick}> </span>
                 <span className='octicon octicon-x'> </span>
                  </div>
           )
